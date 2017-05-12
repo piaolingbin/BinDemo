4ffdfb3ffaf627115c940468838e0d7e7600403e
piaolingbin <cbin0702@163.com>
2017-05-12 10:23:18 +0800

    新加页面

---
+++ app/src/main/java/com/soufun/bin/bindemo/activity/HelloGitActivity.java
@@ -0,0 +1,15 @@
+package com.soufun.bin.bindemo.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.soufun.bin.bindemo.R;
+
+public class HelloGitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_git);
+    }
+}
